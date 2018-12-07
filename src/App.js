94d943c268efe3e654dba1f50ebad228648e4c8e
Peter Krutsiuk <krutsiuk.petr@gmail.com>
2018-12-07 08:26:24 +0200

    initial commit.

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Are you fine?)
           </a>
         </header>
       </div>
